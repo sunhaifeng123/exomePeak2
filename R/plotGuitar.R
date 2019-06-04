791e74286a9dcba947a3e7a832641f0b621f6c3c
sunhaifeng123 <41726638+sunhaifeng123@users.noreply.github.com>
2019-06-04 12:27:14 +0800

    Update plotGuitar.R

--- R/plotGuitar.R
+++ R/plotGuitar.R
@@ -36,10 +36,8 @@
 setMethod("plotGuitar",
           "SummarizedExomePeak",
                function(sep,
-                        txdb = NULL,
                         save_pdf_prefix = NULL,
                         include_control_regions = TRUE,
-                        guitar_coordinate = NULL,
                         save_dir = ".") {
 #Check the installation state of the guitar plot
 if(!require(Guitar)){
@@ -50,11 +48,11 @@ if(sum(grepl("mod",rownames(sep))) < 10 ) {
   stop("guitar plot function cannot be performed for total peaks number < 10.")
 }
 
-stopifnot(!(is.null(txdb) & is.null(guitar_coordinate)))
+# stopifnot(!(is.null(txdb) & is.null(guitar_coordinate)))
 
-if(is.null(guitar_coordinate)){
-  guitar_coordinate = quiet( Guitar::makeGuitarTxdb(txdb) )
-}
+# if(is.null(guitar_coordinate)){
+#   guitar_coordinate = quiet( Guitar::makeGuitarTxdb(txdb) )
+# }
 
 if(is.null(save_pdf_prefix)) {
   dir_arg <- NA
@@ -82,9 +80,9 @@ if(!include_control_regions){
 quiet(
 suppressWarnings(
               GuitarPlot(
-                 gfeatures = gr_list,
-                 GuitarCoordsFromTxDb = guitar_coordinate,
-                 saveToPDFprefix = dir_arg
+                 stGRangeLists = gr_list,
+                 txTxdb = TxDb.Mmusculus.UCSC.mm10.ensGene,
+                 miscOutFilePrefix = dir_arg
                         )
                 )
 )
@@ -124,9 +122,9 @@ suppressWarnings(
    suppressWarnings(
 
                   Guitar::GuitarPlot(
-                  gfeatures = gr_list,
-                  GuitarCoordsFromTxDb = guitar_coordinate,
-                  saveToPDFprefix = dir_arg
+                  stGRangeLists = gr_list,
+                  txTxdb = TxDb.Mmusculus.UCSC.mm10.ensGene,
+                  miscOutFilePrefix = dir_arg
                 )
 
             )
@@ -169,9 +167,9 @@ suppressWarnings(
     suppressWarnings(
       quiet(
       GuitarPlot(
-        gfeatures = gr_list,
-        GuitarCoordsFromTxDb = guitar_coordinate,
-        saveToPDFprefix = dir_arg
+        stGRangeLists = gr_list,
+        txTxdb = TxDb.Mmusculus.UCSC.mm10.ensGene,
+        miscOutFilePrefix = dir_arg
       )
       )
     )
